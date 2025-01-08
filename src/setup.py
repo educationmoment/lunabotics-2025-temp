335f45d2e0de8ed42250aaafa4f1c517cdc67487
Mull <85046208+educationmoment@users.noreply.github.com>
2025-01-07 22:45:33 -0600

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -6,24 +6,24 @@ package_name = 'lunabotics_nodes'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='I'm Stupid',
-    maintainer_email='stupid@stupid.com',
-    description='Example lunabotics node package',
+    maintainer="Fat Loser",  
+    maintainer_email='example@example.com',
+    description='Lunabotics nodes',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # add  executables here 
+            # e.g. 'vision_data_node = lunabotics_nodes.vision_data_node:main',
         ],
     },
 )
